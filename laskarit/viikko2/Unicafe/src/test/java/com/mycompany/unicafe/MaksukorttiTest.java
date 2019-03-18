c3972389cb7da07a8ec18c3c6f51e5147f959b6f
Noora Rytilä <rytilanoora@gmail.com>
2019-03-18 20:19:38 +0200

    lisaykset

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -7,14 +7,42 @@ import org.junit.Test;
 public class MaksukorttiTest {
 
     Maksukortti kortti;
+    Kassapaate kassapaate;
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
+        kassapaate = new Kassapaate();
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void saldoOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void rahanLataaminenToimiiOikein() {
+        kortti.lataaRahaa(500);
+        assertEquals("saldo: 15.0", kortti.toString());
+        
+    }
+    
+    @Test
+    public void riittaakkoRahat() {
+        assertTrue(kassapaate.syoEdullisesti(kortti) == true);
+        
+    }
+    
+    @Test
+    public void rahatEiRiita() {
+        kassapaate.syoMaukkaasti(kortti);
+        kassapaate.syoMaukkaasti(kortti);
+        assertTrue(kassapaate.syoMaukkaasti(kortti) == false);
+                
     }
 }
