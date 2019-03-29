dda2f0a6d989d322d3fff394797a5c27202bbb8e
Noora Rytilä <rytilanoora@gmail.com>
2019-03-29 19:09:23 +0200

    projekti luotu

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -11,45 +11,68 @@ import static org.junit.Assert.*;
 public class KassapaateTest {
     
     Kassapaate kassapaate;
+    Maksukortti kortti;
     
     
     @Before
     public void setUp() {
         kassapaate = new Kassapaate();
+        kortti = new Maksukortti(1000);
     }
-    
-    @Test
-    public void hello() {}
-    
-    
+
     @Test
     public void rahamaaraOikein() {
         assertTrue(kassapaate.kassassaRahaa() == 100000);
     }
     
     @Test
-    public void myydytLounaatOikein() {
-        assertTrue(kassapaate.edullisiaLounaitaMyyty() == 0 && kassapaate.maukkaitaLounaitaMyyty() == 0);
+    public void myydytLounaatOikeinEdulliset() {
+        assertTrue(kassapaate.edullisiaLounaitaMyyty() == 0);
         
     }
     
     @Test
-    public void KassassaOlevaRahamaaraKasvaa() {
+    public void myydytLounaatOikeinMaukkaat() {
+        assertTrue(kassapaate.maukkaitaLounaitaMyyty() == 0);
+        
+    }
+    
+    @Test
+    public void KassassaOlevaRahamaaraKasvaaEdullinen() {
         kassapaate.syoEdullisesti(240);
         assertTrue(kassapaate.kassassaRahaa() == 100240);
     }
     
     @Test
-    public void VaihtorahaOikein() {
+    public void KassassaOlevaRahamaaraKasvaaMaukas() {
+        kassapaate.syoMaukkaasti(400);
+        assertTrue(kassapaate.kassassaRahaa() == 100400);
+    }
+    
+    @Test
+    public void VaihtorahaOikeinMaukas() {
         
         assertTrue(kassapaate.syoMaukkaasti(1000) == 600);
         
     }
     
     @Test
-    public void myytyjenLounaidenMaaraKasvaa() {
+    public void VaihtorahaOikeinEdullinen() {
+        
+        assertTrue(kassapaate.syoEdullisesti(1000) == 760);
+        
+    }
+    
+    @Test
+    public void myytyjenLounaidenMaaraKasvaaEdulliset() {
         kassapaate.syoEdullisesti(240);
         assertTrue(kassapaate.edullisiaLounaitaMyyty() == 1);
+    }
+    
+    @Test
+    public void myytyjenLounaidenMaaraKasvaaMaukkaat() {
+        kassapaate.syoMaukkaasti(400);
+        assertTrue(kassapaate.maukkaitaLounaitaMyyty() == 1);
     }
     
     @Test
@@ -57,5 +80,62 @@ public class KassapaateTest {
         kassapaate.syoEdullisesti(200);
         assertTrue(kassapaate.syoEdullisesti(200) == 200);
         assertTrue(kassapaate.edullisiaLounaitaMyyty() == 0);
+    }
+    
+    @Test
+    public void edullisenOstoKortillaToimii() {
+        assertTrue(kassapaate.syoEdullisesti(kortti));
+        assertTrue(kassapaate.edullisiaLounaitaMyyty() == 1);
+    }
+    
+    @Test
+    public void maukkaanOstoKortillaToimii() {
+        assertTrue(kassapaate.syoMaukkaasti(kortti));
+        assertTrue(kassapaate.maukkaitaLounaitaMyyty() == 1);
+    }
+    
+    @Test
+    public void kortillaEiTarpeeksiRahaaMaukas() {
+        kassapaate.syoMaukkaasti(kortti);
+        kassapaate.syoMaukkaasti(kortti);
+        assertFalse(kassapaate.syoMaukkaasti(kortti));
+        assertTrue(kassapaate.maukkaitaLounaitaMyyty() == 2);
+    }
+    
+    @Test
+    public void kortillaEiTarpeeksiRahaaEdullinen() {
+        kassapaate.syoEdullisesti(kortti);
+        kassapaate.syoEdullisesti(kortti);
+        kassapaate.syoEdullisesti(kortti);
+        kassapaate.syoEdullisesti(kortti);
+        assertFalse(kassapaate.syoEdullisesti(kortti));
+        assertTrue(kassapaate.edullisiaLounaitaMyyty() == 4);
+    }
+    
+    @Test
+    public void rahamaaraEiMuutuKortillaOstettaessaEdullinen() {
+        kassapaate.syoEdullisesti(kortti);
+        assertTrue(kassapaate.kassassaRahaa() == 100000);
+    }
+    
+    @Test
+    public void rahamaaraEiMuutuKortillaOstettaessaMaukas() {
+        kassapaate.syoMaukkaasti(kortti);
+        assertTrue(kassapaate.kassassaRahaa() == 100000);
+    }
+    
+    @Test
+    public void kortinSaldoJaKassanSaldoMuuttuu() {
+        kassapaate.lataaRahaaKortille(kortti, 1000);
+        assertTrue(kortti.saldo() == 2000);
+        assertTrue(kassapaate.kassassaRahaa() == 101000);
+    }
+    
+    @Test
+    public void negatiivisenSaldonLataaminenEiOnnistu() {
+        kassapaate.lataaRahaaKortille(kortti, -1000);
+        assertTrue(kassapaate.kassassaRahaa() == 100000);
+        
+        
     }
 }
